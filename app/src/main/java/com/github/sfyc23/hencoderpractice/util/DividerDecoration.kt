0b35b6d1477a9f5c60a3c12a1d05a33690651397
18865091603 <725412890@qq.com>
2018-10-24 19:24:09 +0800

    sss

--- app/src/main/java/com/github/sfyc23/hencoderpractice/util/DividerDecoration.kt
+++ app/src/main/java/com/github/sfyc23/hencoderpractice/util/DividerDecoration.kt
@@ -17,6 +17,7 @@ import android.util.TypedValue
 import android.view.View
 
 /**
+ *
  * A simple divider decoration with customizable colour, height, and left and right padding.
  */
 class DividerDecoration private constructor(
